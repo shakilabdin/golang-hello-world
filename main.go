9d1fab86007534cff43fdfa028c7e72dbcff5571
Shakil Abdin <shakil.abdin@gmail.com>
2020-10-01 18:58:30 -0400

    done

--- main.go
+++ main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+	foo()
+	fmt.Println("after foo")
+
+	for i := 0; i < 100; i++ {
+		if i %2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("I'm in foo.")
 }
